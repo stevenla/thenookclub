30680c0786680fa3f3fde8eaa4df8af3451e09f5
Steven La <sla@fb.com>
2020-04-07 18:46:48 -0400

    commented out

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,41 @@
-// import React from "react";
-// import CrittersPage from "./CrittersPage";
-// import styles from "./App.module.css";
-
-// export default function App() {
-//   return (
-//     <div className={styles.root}>
-//       <CrittersPage />
-//     </div>
-//   );
-// }
-
 import React from "react";
-import { HashRouter } from "react-router-dom";
-import { Routes, Route } from "react-router";
-
+import CrittersPage from "./CrittersPage";
 import styles from "./App.module.css";
-import Nav from "./Nav";
-import ROUTES from "./Routes";
 
 export default function App() {
   return (
-    <HashRouter>
-      <div className={styles.root}>
-        <Nav />
-        <Routes>
-          {ROUTES.map((routeConfig) => {
-            const Page = routeConfig.page.default;
-            return (
-              <Route
-                key={routeConfig.path}
-                path={routeConfig.path}
-                element={<Page />}
-              />
-            );
-          })}
-        </Routes>
-      </div>
-    </HashRouter>
+    <div className={styles.root}>
+      <CrittersPage />
+    </div>
   );
 }
+
+// import React from "react";
+// import { HashRouter } from "react-router-dom";
+// import { Routes, Route } from "react-router";
+
+// import styles from "./App.module.css";
+// import Nav from "./Nav";
+// import ROUTES from "./Routes";
+
+// export default function App() {
+//   return (
+//     <HashRouter>
+//       <div className={styles.root}>
+//         <Nav />
+//         <Routes>
+//           {ROUTES.map((routeConfig) => {
+//             const Page = routeConfig.page.default;
+//             return (
+//               <Route
+//                 key={routeConfig.path}
+//                 path={routeConfig.path}
+//                 element={<Page />}
+//               />
+//             );
+//           })}
+//         </Routes>
+//       </div>
+//     </HashRouter>
+//   );
+// }
