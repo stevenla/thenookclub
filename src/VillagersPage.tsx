8ec7817096fd1adbea13a9e3c2397409362e2a7e
Steven La <sla@fb.com>
2020-05-22 15:10:56 -0400

    add rudimentary villagers page

--- src/VillagersPage.tsx
+++ src/VillagersPage.tsx
@@ -1,12 +1,42 @@
 import React from "react";
-import styles from "./VillagersPage.module.css";
 import NullState from "./NullState";
 import VillagersIcon from "./icons/VillagersIcon";
+import { css } from "otion";
+
+import VILLAGERS_DATA from "./data/villagers.json";
+
+function nameToImageName(name: string): string {
+  const cleanName = name.toLocaleLowerCase().replace(/[^\s\w\d]/g, "");
+  return `/images/villagers/${cleanName}.png`;
+}
 
 export default function VillagersPage() {
   return (
     <div className={styles.root}>
-      <NullState icon={<VillagersIcon size={64} />} text="Coming soon!" />
+      {VILLAGERS_DATA.map((villager) => {
+        return (
+          <div className={styles.villager} key={villager.name}>
+            {villager.name}
+            <img
+              className={styles.image}
+              src={nameToImageName(villager.name)}
+            />
+          </div>
+        );
+      })}
     </div>
   );
 }
+
+const styles = {
+  root: css({
+    display: "flex",
+    flexDirection: "column",
+    flexGrow: 1,
+  }),
+  villager: css({}),
+  image: css({
+    height: 64,
+    width: 64,
+  }),
+};
